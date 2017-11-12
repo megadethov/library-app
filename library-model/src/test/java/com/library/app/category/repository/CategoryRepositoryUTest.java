4592168612e14090b0673228e7f48704898e3009
Yuriy Yefremov <megadethov@gmail.com>
2017-11-12 11:09:52 +0200

    add test - alreadyExistsCategoryWithId

--- library-model/src/test/java/com/library/app/category/repository/CategoryRepositoryUTest.java
+++ library-model/src/test/java/com/library/app/category/repository/CategoryRepositoryUTest.java
@@ -113,4 +113,20 @@ public class CategoryRepositoryUTest {
         assertThat(categoryRepository.alreadyExists(cleanCode()), is(equalTo(false)));
     }
 
+    @Test
+    public void alreadyExistsCategoryWithId() {
+        final Category java = dBCommandTransactionalExecutor.executeCommand(() -> {
+            categoryRepository.add(cleanCode());
+            return categoryRepository.add(java());
+        });
+
+        assertThat(categoryRepository.alreadyExists(java), is(equalTo(false)));
+
+        java.setName(cleanCode().getName());
+        assertThat(categoryRepository.alreadyExists(java), is(equalTo(true)));
+
+        java.setName(networks().getName());
+        assertThat(categoryRepository.alreadyExists(java), is(equalTo(false)));
+    }
+
 }
